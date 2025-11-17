26590282c2ee1727aaa8399ad8d48139738e95f7
Fabian Steeg <steeg@hbz-nrw.de>
2025-11-17 11:33:47 +0100

    Add hook to fetch almaMmsId, hbzId, and zdbId when saving (#3)

---
+++ src/api/holding/content-types/holding/lifecycles.js
@@ -0,0 +1,24 @@
+const fetch = require("node-fetch");
+
+module.exports = {
+    async beforeUpdate(event) {
+        const data = event.params.data;
+        const [almaMmsId, hbzId, zdbId] =
+            await fetchFields(data.lobidUri, ["almaMmsId", "hbzId", "zdbId"]);
+        data.almaMmsId = almaMmsId;
+        data.hbzId = hbzId;
+        data.zdbId = zdbId;
+    }
+};
+
+const fetchFields = async (lobidUri, fields) => {
+    const url = `${lobidUri.replace("#!", "")}?format=json`;
+    const response = await fetch(url);
+    if (!response.ok) {
+        console.log(`Unexpected response; status ${response.status} for url ${url}`);
+        return null;
+    } else {
+        const json = await response.json();
+        return fields.map(field => (json[field] || ""));
+    }
+}
